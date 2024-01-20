0d86ba10277ff9b26a1857a4ba0b3d174b86a4c8
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-01-20 19:57:09 +0900

    Title: 배열의 합, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240120/배열의 합/sum-of-array.py
+++ 240120/배열의 합/sum-of-array.py
@@ -1,11 +1,7 @@
 # 입력 받기
-A = []
-for _ in range(4):
-    A.append(list(map(int, input().split())))
-
-total = [0,0,0,0]
+A = [
+    sum(list(map(int, input().split())))
+    for _ in range(4)
+]
 for i in range(len(A)):
-    total[i] = (sum(A[i]))
-
-for i in range(len(total)):
-    print(total[i])
+    print(A[i])
