74f7a5b5950e5bf5869073fd4330bd73fb8c23c3
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-07-15 00:22:53 +0900

    Title: 정수 선언하고 곱 출력, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240715/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언
+        int a = 26, b = 5;
+
+        // 출력
+        System.out.print(a + " * " + b + " = " + (a*b));
+    }
+}
