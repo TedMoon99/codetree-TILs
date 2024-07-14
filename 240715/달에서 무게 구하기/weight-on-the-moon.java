e0a15c51de9ca82a2f12e5716df5c6396b77c026
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-07-15 00:44:08 +0900

    Title: 달에서 무게 구하기, Time: 103ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240715/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언
+        int weight = 13;
+        double rate = 0.165;
+
+        // 출력
+        System.out.printf("%d * %f = %.6f", weight, rate, (weight * rate));
+    }
+}
