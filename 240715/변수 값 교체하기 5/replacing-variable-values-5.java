2ef33267a8abb66c3a31af536da4cf09c9c9b63a
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-07-15 02:02:00 +0900

    Title: 변수 값 교체하기 5, Time: 81ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240715/변수 값 교체하기 5/replacing-variable-values-5.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언
+        int a = 2, b = 6;
+
+        // 다시 b의 값을 a에 넣어준다
+        a = b;
+
+        // 출력
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
