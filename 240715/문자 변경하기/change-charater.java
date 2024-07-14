a93a67306568ed92a29a4a843f3d09f8b2bf5e5c
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-07-15 01:55:43 +0900

    Title: 문자 변경하기, Time: 77ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240715/문자 변경하기/change-charater.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언
+        char a = 'C';
+
+        // a에 다시 T를 넣어준다
+        a = 'T';
+        System.out.print(a);
+    }
+}
