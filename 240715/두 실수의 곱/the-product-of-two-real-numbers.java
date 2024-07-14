51b1adf90b7f25ade64339270f050234b188ad4a
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-07-15 00:55:51 +0900

    Title: 두 실수의 곱, Time: 91ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240715/두 실수의 곱/the-product-of-two-real-numbers.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언
+        double a = 5.26, b = 8.27;
+
+        // 두 수의 곱 계산
+        double result = a * b;
+
+        // 출력
+        System.out.printf("%.3f", result);
+    }
+}
