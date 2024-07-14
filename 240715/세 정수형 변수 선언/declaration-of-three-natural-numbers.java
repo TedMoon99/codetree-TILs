ee92c489e9648b9448b70ce69cf5ec353181b3d6
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-07-15 00:33:15 +0900

    Title: 세 정수형 변수 선언, Time: 99ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240715/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언
+        int a= 7, b = 23, c = 30;
+
+        System.out.printf("%d + %d = %d", a, b, c);
+    }
+}
