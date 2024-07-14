123f4316c2115358be11a5ab5cd347561ce66027
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-07-15 02:03:45 +0900

    Title: 정수 복사, Time: 80ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240715/정수 복사/copy-integer.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언
+        int a = 3, b = 4;
+
+        b = a;
+        
+        System.out.print(a);
+        System.out.println(" " + b);
+        System.out.print(a * b);
+    }
+}
