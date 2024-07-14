73889361f9b5ea7d57854581779384fb15003fb7
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-07-15 00:37:07 +0900

    Title: 변수 출력하기 2, Time: 103ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240715/변수 출력하기 2/outputing-variables-2.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언
+        int a = 3;
+        char b = 'C';
+
+        // 출력
+        System.out.printf("%c!.....!%d", b, a);
+
+    }
+}
