a739d0f63d12b589f120fd972f955e40b828eabe
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-07-15 00:21:19 +0900

    Title: 변수 선언하기, Time: 77ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240715/변수 선언하기/declaring-variables.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언
+        int a = 3;
+        char b = 'C';
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
