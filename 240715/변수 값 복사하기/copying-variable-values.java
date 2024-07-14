5a166dd53a0b503ab71afb18226b5d49d3cd7d13
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-07-15 02:24:59 +0900

    Title: 변수 값 복사하기, Time: 85ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240715/변수 값 복사하기/copying-variable-values.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언
+        int a = 1, b = 2, c = 3;
+
+        // 값 동시에 복사
+        a = b = c;
+
+        // 출력
+        System.out.printf("%d %d %d", a, b, c);
+    }
+}
