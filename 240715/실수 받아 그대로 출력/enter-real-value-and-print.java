55b9fd35dc038e51fc0f66ad7de742dcdb234959
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-07-15 23:32:34 +0900

    Title: 실수 받아 그대로 출력, Time: 138ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240715/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Scanner 선언
+        Scanner sc = new Scanner(System.in);
+        // 변수 선언
+        double n = sc.nextDouble();
+        // 출력
+        System.out.printf("%.2f", n);
+    }
+}
