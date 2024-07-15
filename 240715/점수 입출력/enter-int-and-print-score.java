0efe8ebf4bae66efdcf1f9d9b35bddb68bdfdb7f
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-07-15 21:56:31 +0900

    Title: 점수 입출력, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240715/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 스캐너 선언
+        Scanner sc = new Scanner(System.in);
+        // 변수 선언
+        int n = sc.nextInt();
+
+        // 출력
+        System.out.printf("Your score is %d point.", n);
+    }
+}
