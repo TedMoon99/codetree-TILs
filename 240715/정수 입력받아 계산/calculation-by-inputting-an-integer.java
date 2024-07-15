5d23cbfe796660c4379ea6a0e4a026bdc03754f1
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-07-15 23:27:24 +0900

    Title: 정수 입력받아 계산, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240715/정수 입력받아 계산/calculation-by-inputting-an-integer.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 스캐너 선언
+        Scanner sc = new Scanner(System.in);
+        // 변수 선언
+        int a = sc.nextInt();
+        // 출력
+        System.out.print(a * 2 + 3);
+    }
+}
