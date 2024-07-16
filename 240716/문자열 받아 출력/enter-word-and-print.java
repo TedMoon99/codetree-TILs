037af06d58b8654435e951e805b34a6847dc274c
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-07-16 18:01:19 +0900

    Title: 문자열 받아 출력, Time: 111ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240716/문자열 받아 출력/enter-word-and-print.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Scanner
+        Scanner sc = new Scanner(System.in);
+        // 입력받기
+        String s = sc.next();
+        // 출력
+        System.out.println(s);
+    }
+}
