e055e312a4d4dba7fd6afab58968e9b29a6ecfba
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-07-17 00:06:02 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240717/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++){ print5Stars();}
+    }
+
+    public static void print5Stars(){
+        for(int i = 0; i < 10; i++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
