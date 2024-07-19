c76c65ab81c9bdf52e56e90e03680bea76051452
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-07-19 20:11:36 +0900

    Title: 입력받아 연산하기, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240719/입력받아 연산하기/take-input-and-operate.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args){
+        // Scanner
+        Scanner sc = new Scanner(System.in);
+        // input
+        int a = sc.nextInt(), b = sc.nextInt();
+        // output
+        System.out.println(a+87);
+        System.out.println(b % 10);
+    }
+}
