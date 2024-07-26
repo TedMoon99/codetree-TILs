c5f925267c708aea15df76339cbfee41a300605c
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-07-26 20:01:46 +0900

    Title: 문자열 정렬, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240726/문자열 정렬/string-sort.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char[] chars = str.toCharArray();
+        Arrays.sort(chars);
+        String sortedStr = new String(chars);
+        System.out.println(sortedStr);
+    }
+}
