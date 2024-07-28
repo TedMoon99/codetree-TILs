838427ad92fc0accae901280eec2e59f331e0f2a
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-07-28 19:04:36 +0900

    Title: 동전이 있는 위치, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240728/동전이 있는 위치/Where-coins-are-located.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt();
+        int[][] A = new int[n+1][n+1];
+        // Set Value
+        for (int i = 1; i <= m; i++){
+            int row = sc.nextInt();
+            int column = sc.nextInt();
+
+            A[row][column] = 1;
+        }
+        // output
+        for(int i = 1; i < n+1; i++){
+            for (int j = 1; j < n+1; j++){
+                System.out.print(A[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
