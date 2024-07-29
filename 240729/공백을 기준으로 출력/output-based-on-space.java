3969d06e3bbcf515062f7aa3bf23b53b45e775ed
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-07-29 18:51:47 +0900

    Title: 공백을 기준으로 출력, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240729/공백을 기준으로 출력/output-based-on-space.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input1 = sc.nextLine().replace(" ", ""), input2 = sc.nextLine().replace(" ", "");
+
+        // 출력한다
+        System.out.println(input1+input2);
+    }
+}
