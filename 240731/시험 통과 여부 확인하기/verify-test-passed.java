67c378f423f277a2f8c002772061eac9bd0f3910
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-07-31 21:46:57 +0900

    Title: 시험 통과 여부 확인하기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240731/시험 통과 여부 확인하기/verify-test-passed.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        if (n >= 80) System.out.println("pass");
+        else System.out.printf("%d more score", 100-n);
+    }
+}
