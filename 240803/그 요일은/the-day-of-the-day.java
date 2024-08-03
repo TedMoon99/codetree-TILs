fa6b1c4bd8d5c4c46331545666b8102d28427246
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-08-03 17:16:01 +0900

    Title: 그 요일은, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240803/그 요일은/the-day-of-the-day.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] dateList = new int[]{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] dowList = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+        // input
+        int m1 =sc.nextInt(), d1 = sc.nextInt(), m2 = sc.nextInt(), d2 = sc.nextInt();
+        String target = sc.next();
+
+        int first = 0, last = 0, gapOfDate = 0;
+        // 날짜 차이 계산
+        for (int i = 0; i < m1; i++){
+            first += dateList[i];
+        }
+        for (int i = 0; i < m2; i++){
+            last += dateList[i];
+        }
+        first += d1;
+        last += d2;
+        gapOfDate = last - first;
+
+        // m1월 d1일의 요일 == m2월 d2일의 요일 == target
+        if (dowList[findDow(gapOfDate)].equals(target) && target.equals("Mon")){
+            System.out.println(findWeekNumber(gapOfDate)+2);
+        } else if (dowList[findDow(gapOfDate)].equals(target)){
+            System.out.println(findWeekNumber(gapOfDate)+1);
+        } else {
+            System.out.println(findWeekNumber(gapOfDate));
+        }
+    }
+
+    // 날짜수의 차이를 입력하여 무슨 요일인지 찾는다
+    private static int findDow(int gapOfDate){
+        while (gapOfDate < 0){
+            gapOfDate += 7;
+        }
+        return gapOfDate % 7;
+    }
+    // 날짜수의 차이를 입력하여 몇 주의 차이가 나는지 찾는다
+    private static int findWeekNumber(int gapOfDate){
+        int count = 0;
+        while (gapOfDate > 6){
+            count++;
+            gapOfDate -= 7;
+        }
+        return count;
+    }
+
+}
