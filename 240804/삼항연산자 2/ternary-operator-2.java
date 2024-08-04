53d087cf1f142dd428f2ba151bfa9c8f3a62c9f3
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-08-04 22:22:15 +0900

    Title: 삼항연산자 2, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240804/삼항연산자 2/ternary-operator-2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        String result = (a == 1) ? "t" : "f";
+        System.out.println(result);
+    }
+}
