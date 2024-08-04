4c53542b12c9e90c4e4943b46fc55d521e1f9fb3
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-08-04 22:00:49 +0900

    Title: 2개 중 최대, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240804/2개 중 최대/max-of-two-nums.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+        int max = Math.max(a, b);
+        System.out.println(max);
+    }
+}
