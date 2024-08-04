9c6ebe724da095fbe30849cb3bb04feb62659b81
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-08-04 22:12:04 +0900

    Title: 삼항연산자, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240804/삼항연산자/ternary-operator.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        if(n == 100) System.out.println("pass");
+        else System.out.println("failure");
+    }
+}
