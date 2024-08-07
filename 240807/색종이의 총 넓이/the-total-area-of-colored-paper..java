2c883a1392d53216ec3a9618909cd80f8b88265d
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-08-07 18:43:30 +0900

    Title: 색종이의 총 넓이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240807/색종이의 총 넓이/the-total-area-of-colored-paper..java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] square = new int[201][201];
+        // 색종이 갯수
+        int n = sc.nextInt();
+        Coordinate[] startList = new Coordinate[n];
+        Coordinate[] endList = new Coordinate[n];
+        // 좌표 입력 받기
+        for (int i = 0; i < n; i++){
+            // 좌표 입력
+            int x1 = sc.nextInt(), y1 = sc.nextInt();
+            int x2 = x1 + 8, y2 = y1 + 8;
+            // 객체 생성
+            Coordinate bottomLeft = new Coordinate(x1, y1);
+            Coordinate topRight = new Coordinate(x2, y2);
+            // 리스트에 입력
+            startList[i] = bottomLeft;
+            endList[i] = topRight;
+        }
+        // 넓이 영역 생성
+        for (int s = 0; s < 3; s++){
+            // 넓이 영역 계산
+            for (int i = startList[s].x; i < endList[s].x; i++){
+                for (int j = startList[s].y; j < endList[s].y; j++){
+                    square[i][j] = 1;
+                }
+            }
+        }
+        // A: 1, B: 2, M: 3
+        // 넓이 영역 계산
+        int s = 0;
+        for (int i = 0; i < 201; i++){
+            for (int j = 0; j < 201; j++){
+                // 빈 공간이 아님 and M 영역이 아님 s++
+                if (square[i][j] == 1) s++;
+            }
+        }
+        // 출력
+        System.out.print(s);
+
+    }
+}
+
+class Coordinate{
+    int x, y;
+    Coordinate(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
