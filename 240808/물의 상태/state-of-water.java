6d6ae9376e2d0d90295cd19e3c7091a03e4eb1eb
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-08-08 16:50:45 +0900

    Title: 물의 상태, Time: 189ms, Memory: 10MB, Status: Passed - CodeTree

--- 240808/물의 상태/state-of-water.java
+++ 240808/물의 상태/state-of-water.java
@@ -7,6 +7,6 @@ public class Main {
         int n = sc.nextInt();
         if (n < 0) System.out.print("ice");
         else if ( n < 100) System.out.print("water");
-        else System.out.print("water");
+        else System.out.print("vapor");
     }
 }
