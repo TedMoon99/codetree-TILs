cbfb7648c7a961eb5c61531e66b9c3897d3cf443
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-08-10 18:55:16 +0900

    Title: 최대로 겹치는 지점, Time: 151ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240810/최대로 겹치는 지점/maximum-overlapped-points.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        Tile[] interval = new Tile[150];
+        for (int i = 0; i < interval.length; i++){
+            interval[i] = new Tile(i);
+        }
+        for (int i = 0; i < n; i++){
+            int x1 = sc.nextInt(), x2 = sc.nextInt();
+            for (int j = x1; j <= x2; j++){
+                interval[i].count++;
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < interval.length; i++){ // 100 ~ 200 확인
+            if (interval[i].count > max) max++;
+        }
+        // 출력
+        System.out.print(max);
+    }
+}
+
+class Tile{
+    int count = 0, index;
+    // 생성자
+    Tile(int position){
+        this.index = count;
+    }
+}
