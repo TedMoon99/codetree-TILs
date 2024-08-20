439ba6407fb8c3cfd4b570eb051c494e514232a4
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-08-20 21:55:36 +0900

    Title: 살 수 있는 물건, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240820/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        if (n >= 3000)
+        System.out.println("book");
+        else if (n >= 1000)
+        System.out. println("mask");
+        else
+        System.out.println("no");
+
+    }
+}
