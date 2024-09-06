73a14e14719e88d1db22f8f6f5f26d2b2fbe5369
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-09-06 21:30:21 +0900

    Title: 시력 검사, Time: 167ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240906/시력 검사/eye-test.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble(), b = sc.nextDouble();
+
+        if(a >= 1.0  && b >= 1.0) System.out.println("High");
+        else if(a >= 0.5  && b >= 0.5) System.out.println("Middle");
+        else System.out.println("Low");
+    }
+}
