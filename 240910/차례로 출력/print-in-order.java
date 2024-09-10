e67e03d0ca274f966e0d94f5858fe15eafd42a48
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-09-10 20:26:07 +0900

    Title: 차례로 출력, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240910/차례로 출력/print-in-order.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for(int i = 5; i < 18; i++){
+            System.out.print(i + " ");
+        }
+    }
+}
