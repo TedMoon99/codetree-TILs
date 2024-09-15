af672385fb13217649dca0a7bab8e9d5fed55648
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-09-15 20:21:32 +0900

    Title: 100점 도달하기, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240915/100점 도달하기/reach-100-point.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static int score;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        score = sc.nextInt();
+
+        for (int i = score; i < 100; i++){
+            fun(i);
+        }
+    }
+
+    private static void fun(int score){
+        String result = "";
+        if (score >= 90) result = "A";
+        else if (score >= 80) result = "B";
+        else if (score >= 70) result = "C";
+        else if (score >= 60) result = "D";
+        else result = "F";
+
+        System.out.println(result + " ");
+    }
+}
