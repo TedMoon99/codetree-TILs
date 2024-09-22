a09817d2a0bbc8ca56067c8c120aa510e37c9341
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-09-22 17:21:11 +0900

    Title: 특정 문자로 끝나는 문자열, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240922/특정 문자로 끝나는 문자열/string-ending-with-specific-character.java
+++ 240922/특정 문자로 끝나는 문자열/string-ending-with-specific-character.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static String[] arr = new String[10];
     public static char target;
+    public static int cnt = 0;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -14,7 +15,12 @@ public class Main {
         target = sc.next().charAt(0);
 
         for (int i = 0; i < 10; i++){
-            if (arr[i].charAt(arr[i].length()-1) == target) System.out.println(arr[i]);
+            if (arr[i].charAt(arr[i].length()-1) == target){
+                System.out.println(arr[i]);
+                cnt++;
+            }
         }
+
+        if(cnt == 0) System.out.println("None")
     }
 }
