ad3024615242ef6c230b4a1266c9df3cab415669
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-09-22 16:32:54 +0900

    Title: 단어로 구분하기 3, Time: 114ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240922/단어로 구분하기 3/separate-words-with-words-3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        String[] arr = new String[10];
+        for (int i = 0; i < 10; i ++){
+            arr[i] = sc.next();
+        }
+
+        for(int i = 9; i >=0; i--){
+            System.out.println(arr[i]);
+        }
+    }
+}
