73f0a36980bf38f1d403143e39645f1dccbb1afe
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-09-22 16:28:32 +0900

    Title: 단어로 구분하기, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240922/단어로 구분하기/separate-words-with-words.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 10; i++){
+            System.out.println(sc.next());
+        }
+    }
+}
