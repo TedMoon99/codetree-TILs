815ea9722c0aa8c211785c314dc13f0f7908c272
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-09-22 18:13:57 +0900

    Title: 문자열 추가하기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240922/문자열 추가하기/add-spring.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        StringBuilder result = new StringBuilder();
+
+        result.append(sc.next()).append("Hello");
+        System.out.println(result);
+    }
+}
