fc695736fe7674896a33f86e49f721d0ba166cbe
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-09-25 19:57:53 +0900

    Title: 감소시키며 출력하기, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240925/감소시키며 출력하기/decrease-and-print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 17; i >4;i--){
+            System.out.print(i + " ");
+        }
+    }
+}
