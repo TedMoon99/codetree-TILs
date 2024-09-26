8e30d13a0f7a63736569f3dcb57ae592e377d77d
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-09-26 20:30:09 +0900

    Title: 증가시키며 출력하기 2, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240926/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int num = 10;
+
+        while(num <= 26){
+            System.out.print(num + " ");
+            num++;
+        }
+
+
+    }
+}
