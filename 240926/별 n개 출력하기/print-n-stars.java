ec1390d6d054411f32487a9421787e2f30b54a3e
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-09-26 20:33:39 +0900

    Title: 별 n개 출력하기, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240926/별 n개 출력하기/print-n-stars.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+
+    public static int n;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        int i = 0;
+        while(i < n){
+            System.out.println("*");
+            i++;
+        }
+    }
+}
