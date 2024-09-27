434043291a906ff97840ebad422d0b67803ef16c
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-09-27 22:23:05 +0900

    Title: 증가시키며 출력하기 3, Time: 3027ms, Memory: 26MB, Status: Time Limit Exceed - CodeTree

---
+++ 240927/증가시키며 출력하기 3/increase-and-print-3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 10;
+        while(i <= 26){
+            System.out.print(i + " ");
+        }
+
+
+    }
+}
