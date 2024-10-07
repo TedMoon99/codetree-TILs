5aebbb8c1d1a17df6dc646694d1053fae3fd2c8b
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-10-07 22:44:21 +0900

    Title: 감소시키며 출력하기 2, Time: 84ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241007/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 26;
+
+        while(i >= 10){
+            System.out.print(i-- + " ");
+        }
+    }
+}
