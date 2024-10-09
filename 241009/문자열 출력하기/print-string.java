f07c5cf340730033947b3811fe1c94353d35daca
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-10-09 21:39:15 +0900

    Title: 문자열 출력하기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241009/문자열 출력하기/print-string.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+
+    static final String fixString = "LeebrosCode";
+    static int n;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        for (int i =0 ; i< n; i++){
+            System.out.println(fixString);
+        }
+    }
+}
