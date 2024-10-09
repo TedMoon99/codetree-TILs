00173ec8b3c722ca0f74e4fac2568b82d3bd784e
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-10-09 21:42:22 +0900

    Title: n번 더하기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241009/n번 더하기/add-n-times.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++){
+            a += n;
+            System.out.println(a);
+        }
+
+    }
+}
