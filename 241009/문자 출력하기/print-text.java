fa6ffc5fcc846d9250cccbda83812ff452c5252e
문태진 <128672219+TedMoon99@users.noreply.github.com>
2024-10-09 21:37:40 +0900

    Title: 문자 출력하기, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241009/문자 출력하기/print-text.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.next();
+
+        for (int i = 0; i < 8; i++){
+            System.out.print(input);
+        }
+    }
+}
